b1cb16c199ac6f53fd82bbcab6d937563425a85b
Mitch Dzugan <mitchdzugan@gmail.com>
2026-01-08 19:19:12 -0600

    fennel proj util

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Empty Template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: {
+    mkZnFnl = (pname: version: mkLuaDeps: srcPath:
+      let
+        mkLua = luaPkgs: (luaPkgs.lua.withPackages mkLuaDeps);
+        mkMacroPath = luaPkgs: (
+          builtins.concatStringsSep
+          ";"
+          (map (pkg: "${pkg}/macro-path/?.fnl") (mkLuaDeps luaPkgs))
+        );
+        mkPkg = luaPkgs: luaPkgs.buildLuarocksPackage {
+          pname = pname;
+          version = version;
+          src = srcPath;
+          buildInputs = [(mkLua luaPkgs)];
+          preBuild = ''
+            rm -rf dist
+            mkdir dist
+            fennel --compile src/ctx.fnl > dist/ctx.lua
+          '';
+          postInstall = ''
+            cp -r macro-path $out
+          '';
+          knownRockspec = "${srcPath}/rockspecs/${pname}-${version}.rockspec";
+          disabled = (luaPkgs.luaOlder "5.1") || (luaPkgs.luaAtLeast "5.4");
+        }; in { mkPkg = mkPkg; } // flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+          luaPkgs = pkgs.lua5_1.pkgs;
+          nativeBuildInputs = with pkgs; [];
+          buildInputs = with pkgs; [
+              (mkLua luaPkgs)
+              rlwrap
+            ];
+        in {
+          packages.default = mkPkg pkgs.lua5_1.pkgs;
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs buildInputs;
+            shellHook = ''
+              function getFlakeRoot {
+                d=$(pwd)
+                if [[ -f "$d/flake.nix" ]]; then
+                  echo $d
+                elif [[ "$d" == "/" ]]; then
+                  echo $1
+                else
+                  cd ..
+                  getFlakeRoot $1
+                fi
+              }
+              function f {
+                rlwrap fennel "$@"
+              }
+              export FLAKE_ROOT=$(getFlakeRoot $(pwd))
+              export FENNEL_MACRO_PATH="$FLAKE_ROOT/macro-path/?.fnl;${mkMacroPath(luaPkgs)}"
+              export FENNEL_PATH="$FLAKE_ROOT/src/?.fnl"
+            '';
+          };
+        }
+      )
+    );
+  };
+}
